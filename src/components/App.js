343affbdf0d73a0350b8abc3461e38039effb6a4
oliver-jk-redding <oliver.jk.redding@gmail.com>
2016-05-24 11:39:28 +1200

    set up deployment branch

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 
-import routes from '../routes'
+import Home from './Home'
 
 export default class App extends Component {
 
   render () {
     return (
-      <div>{routes}</div>
+      <Home/>
     )
   }
 
