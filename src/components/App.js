d9da160d3feb191115867e40c27a09f8789a064c
oliver-jk-redding <oliver.jk.redding@gmail.com>
2016-05-25 23:28:30 +1200

    set text to wait for image loaded

--- src/components/App.js
+++ src/components/App.js
@@ -8,20 +8,26 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      imageHasRendered: false
+      imageStatus: null
     }
   }
 
-  componentDidMount() {
-    this.setState({imageHasRendered: true})
+  handleImageLoaded() {
+    console.log('loaded')
+    this.setState({imageStatus: <Home/>})
+  }
+
+  handleImageErrored() {
+    console.log('not loaded')
+    this.setState({imageStatus: <Home/>});
   }
 
   render () {
     return (
       <div>
-        {this.state.imageHasRendered ? <Home/> : <img src='../../public/images/hex-loader2.gif'/>}
         {this.props.introIsRendered ? <div className='heading-container'><h1 className='animated bounceInLeft'>oliver redding - web developer</h1></div> : null}
-        <div className='image-container'></div>
+        <div className='image-container'><img className='hidden' src='https://lh6.googleusercontent.com/-8yZ_d_xfKG0/V0OAKvU_gqI/AAAAAAAAADI/YvZeA4Y7hxc4RQqk3JEBMnRoEBlFSK8ZwCL0B/w492-h537-no/cutout.png' onLoad={this.handleImageLoaded.bind(this)} onError={this.handleImageErrored.bind(this)}/></div>
+        {this.state.imageStatus}
       </div>
     )
   }
